2f759689c52b10f7e55336cd1baadb84af37fd2b
JonCabot <jfc0053@gmail.com>
2022-04-04 21:50:18 -0500

    completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,33 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    #self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])  
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row| #>>passing in the "sql" from above to our DB hash located in the environment file
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name) #pass in a name we want to search to find database raw data, and thus return as an array by turning it back into a Ruby object
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row| #the return value will be an array (cause ya know, map!)
+      self.new_from_db(row)
+    end.first #this grabs the "first" element in the array returned by the map.  Known as a "chain" to do that.
+  end
+
 end
